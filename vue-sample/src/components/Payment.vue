86385feeaf8a015e410ea8c8d9e9dfc413bd4df8
aryzae <a.aryzae@gmail.com>
2022-07-28 17:33:17 +0900

    commit course 20

---
+++ vue-sample/src/components/Payment.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+const itemName1 = 'Desk'
+const itemName2 = 'Bike'
+
+const price1 = 40000
+const price2 = 20000
+
+const url1 = 'https://www.amazon.co.jp/dp/B09QMBH5R7'
+
+const buy = (itemName: string) => {
+    alert('Are you sure to buy ' + itemName + ' ?')
+}
+</script>
+
+<template>
+    <div class="container">
+        <h1>Payment</h1>
+        <div class="payment">
+            <label>{{ itemName1 }}</label>
+            <label>{{ price1 }} 円</label>
+            <a v-bind:href="url1">bought at ...</a>
+            <button v-on:click="buy(itemName1)">BUY</button>
+        </div>
+        <div class="payment">
+            <label>{{ itemName2 }}</label>
+            <label>{{ price2 }} 円</label>
+            <button v-on:click="buy(itemName2)">BUY</button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+label {
+    font-size: 20px;
+    font-weight: bold;
+}
+
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.payment {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 80px;
+    width: 400px;
+    background-color: aliceblue;
+    margin-bottom: 8px;
+}
+</style>
